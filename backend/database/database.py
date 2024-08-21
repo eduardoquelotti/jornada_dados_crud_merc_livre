0b3e05fbf0bcd0516cd5019ccbeba9e0590ae615
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-08-21 13:09:16 -0300

    Commit atualizado

---
+++ backend/database/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Declarar URL do banco
+POSTGRES_DATA_BASE_URL = "postgresql://user:password@postgres/mydatabase"
+
+# Criar a engine
+engine = create_engine(POSTGRES_DATA_BASE_URL)
+
+# Criar a sessão do banco
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Criar a Base do ORM
+Base = declarative_base()
+
+# Declarar função para gerar sessões (substitui o uso do with)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
---
+++ backend/schema/product.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, PositiveFloat, EmailStr, validators
+from enum import Enum
+from datetime import datetime
+from typing import Optional
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: PositiveFloat
+    categoria: str
+    email_fornecedor: EmailStr
+
+class ProductCreate(ProductBase): # Herdará o schema da class ProductBase e não terá nenhuma particularidade, por isso somente o pass
+    pass
+
+class ProductResponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    categoria: Optional[str] = None
+    email_fornecedor: Optional[EmailStr] = None
